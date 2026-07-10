93490dd1bec23938ff09f414942a157e3faea472
Ailén <ailenechaguibel@gmail.com>
2026-07-10 11:05:36 +0200

    creados los comentarios de la camarera, el prompt con la eleccion de los entrantes y el manejo de errores

--- app.js
+++ app.js
@@ -6,14 +6,63 @@ console.log("Hola la hora detectada es:", horaActual)
 
 const menuLunch = {
     entrantes: {
-        'croquetas': 9.0,
-        'rabas': 8.5,
-        'pulpo': 20.0
+        'Ensaladilla Rusa': 8.50,
+        'Ensalada Mixta': 10.0,
+        'Calamares': 9.0
     },
     extras: {
-        'alioli': 1.0,
-        'pan': 1.0
+        'Alioli': 1.0,
+        'Pan': 1.0
     }
 };
 
-console.log('El precio de la comida es:', menuLunch.entrantes['rabas']);
+const comentariosCamarera = [
+    '¡Excelente elección! De los preferidos de los clientes',
+    '¡Marchando una! Es el especial del chef',
+    '¡Buena elección! El día lo amerita',
+    '¡Perfecto! Segundo que te va a encantar'
+]
+
+let textoMenu = "---NUESTROS ENTRANTES---\n\n\n";
+
+for(let plato in menuLunch.entrantes) {
+    let precio = menuLunch.entrantes[plato];
+
+    textoMenu += `• ${plato}: ${precio}€\n`;
+}
+
+textoMenu += "\n¿Qué te apetece comer hoy?";
+
+//control de errores
+let eleccionUsuario = '';
+let esPlatoValido = false;
+let precioPlato = 0;
+
+while(esPlatoValido === false) {
+    let entrada = prompt(textoMenu);
+
+    if(!entrada) {
+        alert('Por favor elige una opción para que podamos atenderte');
+        continue;
+    }
+
+    let entradaLimpia = entrada.toLowerCase().trim();
+
+    for(let plato in menuLunch.entrantes) {
+        if(plato.toLowerCase() === entradaLimpia) {
+            esPlatoValido = true;
+            eleccionUsuario = plato;
+            precioPlato = menuLunch.entrantes[plato];
+            break;
+        }
+    }
+    if(!esPlatoValido) {
+        alert(`Lo siento, el plato ${entrada} no está en el menu. Elige otra opción`)
+        }
+    }
+
+const indiceAleatorio = Math.floor(Math.random() * comentariosCamarera.length);
+const comentarioElegido = comentariosCamarera[indiceAleatorio];
+
+alert(`${comentarioElegido}`)
+
